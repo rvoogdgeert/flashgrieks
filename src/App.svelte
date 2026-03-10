b87fd1276877219bcb14d0620b7ae2b8ee64014d
Robert Voogdgeert <rvoogdgeert@gmail.com>
2026-03-10 18:04:05 +0100

    Correct mistakes in kappa

--- src/App.svelte
+++ src/App.svelte
@@ -111,10 +111,10 @@
 
         // Κ
         { id: 87, question: "ὑμετέρος", answer: "uw" },
-        { id: 88, question: "ὑμετέρος", answer: "onze" },
+        { id: 88, question: "ἡμετέρος", answer: "onze" },
         { id: 89, question: "μητήρ, ἡ", answer: "moeder" },
         { id: 90, question: "ὀλίγος", answer: "weinig" },
-        { id: 91, question: "μετά", answer: "zeer" },
+        { id: 91, question: "μεγά", answer: "zeer" },
         { id: 92, question: "εὖ", answer: "goed" },
         { id: 93, question: "πόλις, ἡ", answer: "stad" },
         { id: 94, question: "κατά", answer: "van af" },
