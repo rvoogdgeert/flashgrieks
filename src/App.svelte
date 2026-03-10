42c788525ea629a3f0c3780a6457bde84cf2ba37
Robert Voogdgeert <rvoogdgeert@gmail.com>
2026-03-10 08:58:50 +0100

    Only card A

--- src/App.svelte
+++ src/App.svelte
@@ -3,17 +3,17 @@
 
     // Define flash cards data structure
     const flashCards = [
-        //// A
-        //{ id: 1, question: "οὐ, οὐκ, οὐχ", answer: "niet" },
-        //{ id: 2, question: "εἰμι", answer: "zijn" },
-        //{ id: 3, question: "ἄνθρωπος, ὁ", answer: "mens" },
-        //{ id: 4, question: "θεός, ο/ἡ", answer: "god" },
-        //{ id: 5, question: "δουλος", answer: "slaaf" },
-        //{ id: 6, question: "τί;", answer: "wat" },
-        //{ id: 7, question: "ἔχω", answer: "hebben" },
-        //{ id: 8, question: "δε, δ'", answer: "en" },
-        //{ id: 9, question: "φέρω", answer: "dragen" },
-        //{ id: 10, question: "βαίνω", answer: "gaan" },
+        // A
+        { id: 1, question: "οὐ, οὐκ, οὐχ", answer: "niet" },
+        { id: 2, question: "εἰμι", answer: "zijn" },
+        { id: 3, question: "ἄνθρωπος, ὁ", answer: "mens" },
+        { id: 4, question: "θεός, ο/ἡ", answer: "god" },
+        { id: 5, question: "δουλος", answer: "slaaf" },
+        { id: 6, question: "τί;", answer: "wat" },
+        { id: 7, question: "ἔχω", answer: "hebben" },
+        { id: 8, question: "δε, δ'", answer: "en" },
+        { id: 9, question: "φέρω", answer: "dragen" },
+        { id: 10, question: "βαίνω", answer: "gaan" },
 
         //// B
         //{ id: 11, question: "προς", answer: "naar" },
@@ -155,17 +155,17 @@
         //{ id: 123, question: "κελευω", answer: "bevelen" },
         //{ id: 124, question: "κρατεω +gen", answer: "overwinnen" },
 
-        // Ξ
-        { id: 125, question: "τεκνον, το", answer: "kind" },
-        { id: 126, question: "λειπω", answer: "verlaten" },
-        { id: 127, question: "νησος, ἡ", answer: "eiland" },
-        { id: 128, question: "πλεω", answer: "varen" },
-        { id: 129, question: "γαμος, ὁ", answer: "huwelijk" },
-        { id: 130, question: "χαιρεω +dat", answer: "blij zijn met" },
-        { id: 131, question: "παλιν", answer: "weer" },
-        { id: 132, question: "μενω", answer: "blijven" },
-        { id: 133, question: "δακρυω", answer: "huilen" },
-        { id: 134, question: "καθευδω", answer: "slapen" },
+        //// Ξ
+        //{ id: 125, question: "τεκνον, το", answer: "kind" },
+        //{ id: 126, question: "λειπω", answer: "verlaten" },
+        //{ id: 127, question: "νησος, ἡ", answer: "eiland" },
+        //{ id: 128, question: "πλεω", answer: "varen" },
+        //{ id: 129, question: "γαμος, ὁ", answer: "huwelijk" },
+        //{ id: 130, question: "χαιρεω +dat", answer: "blij zijn met" },
+        //{ id: 131, question: "παλιν", answer: "weer" },
+        //{ id: 132, question: "μενω", answer: "blijven" },
+        //{ id: 133, question: "δακρυω", answer: "huilen" },
+        //{ id: 134, question: "καθευδω", answer: "slapen" },
 
         //// Ο
         //{ id: 135, question: "αἰρω", answer: "optillen" },
